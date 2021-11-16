b9661734660640243f7d63356b057ad26f05407c
Sam Armstrong <samfm@me.com>
2021-11-16 11:15:37 -0800

    "Using Redux"

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+<Provider store={store}>
+          <Main />
+      </Provider>
 
 export default function App() {
   return (
--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from "react-native-elements";
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 import { FlatList } from "react-native-gesture-handler";
 import { PARTNERS } from '../shared/partners';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
 
 function Mission() { 
         return (
@@ -29,7 +38,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         }
@@ -40,11 +49,11 @@ class About extends Component {
                     title="Community Partners"
                     >
                         <Mission />
-                    <FlatList
-                        data={this.state.partners}
+                        <FlatList 
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
-                    />  
+                    />
                 </Card>
             </ScrollView>
         
@@ -57,4 +66,4 @@ class About extends Component {
 
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
