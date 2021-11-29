d855fad364f73ae82d5f97193e135b590d702fdc
Sam Armstrong <samfm@me.com>
2021-11-29 13:40:52 -0800

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
-import { ScrollView, Text } from 'react-native';
-import { Card } from "react-native-elements";
+import { ScrollView, Text } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
-    constructor(props){
-        super(props)
-    }
-    
-    static navigationOptions = {
-        title: 'Contact Us'
-    };
-    
-    render() {
-        return(
-        <ScrollView> 
-            <Card
-                title="Contact Information"
-                wrapperStyle={{margin: 20}}
-                >
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-                <Text>1 Nucamp Way</Text> 
-                <Text>Seattle, WA 98001</Text> 
-                <Text>U.S.A.</Text>
-                <Text style={{marginBottom: 5}}></Text>
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
-                
-            </Card>
+  static navigationOptions = {
+    title: "Contact Us",
+  };
 
-        </ScrollView>
-        )
-    }
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ['campsites@nucamp.co'],
+      subject: 'Inquiry',
+      body: 'To whom it may concern:'
+    })
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+            <Button 
+              title="Send Email"
+              buttonStyle={{backgroundColor: "#5637DD", margin: 40}}
+              icon={<Icon
+                name='envelope-o'
+                type='font-awesome'
+                color='#fff'
+                iconStyle={{marginRight: 10}}
+              />}
+              onPress={() => this.sendMail()}
+            />
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
